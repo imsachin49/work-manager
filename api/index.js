df4d35ca1969e6b87489c792716d8d68624f4aa7
Sachin <kumaraditya7125@gmail.com>
2023-05-09 23:02:42 +0530

    done2

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://work-manager-u8s2.vercel.app'],
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization'
 }));
